8a07ae6e26f248f3058a549636b6e6e14c8f02c4
Moussa Mallé <codeangel223@gmail.com>
2026-04-18 12:02:04 +0000

    chore/retire-gemma-ai

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 import os
 from src.glm_ai import Z_AI_Question_Generator
-from src.gemma_ai import Gemma_Question_Generator
+# from src.gemma_ai import Gemma_Question_Generator
 from src.generate_question_handler import GenerateQuestionFromCVCommandHandler, GenerateQuestionFromCVHandler
 from src.api import GenerateQuestionsFromCVDto, GenerateQuestionsFromCVSchema
 
@@ -16,13 +16,13 @@ GENERATE_CV_QUESTION_PROMPT_FILE = os.getenv(
 with open(GENERATE_CV_QUESTION_PROMPT_FILE, "r") as prompt_file:
     GENERATE_CV_PROMPT = prompt_file.read()
 
-gemma_question_generator: Gemma_Question_Generator | None = None
+# gemma_question_generator: Gemma_Question_Generator | None = None
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global gemma_question_generator
-    gemma_question_generator = Gemma_Question_Generator(GENERATE_CV_PROMPT)
+    # global gemma_question_generator
+    # gemma_question_generator = Gemma_Question_Generator(GENERATE_CV_PROMPT)
     yield
 
 
@@ -53,15 +53,15 @@ async def generate_questions_from_cv(data: GenerateQuestionsFromCVSchema):
     return GenerateQuestionsFromCVDto(question=question_generated)
 
 
-@app.post("/gemma/generate-question-from-cv", response_model=GenerateQuestionsFromCVDto)
-async def gemma_generate_questions_from_cv(data: GenerateQuestionsFromCVSchema):
-    assert gemma_question_generator is not None
-    handler = GenerateQuestionFromCVHandler(gemma_question_generator)
-    command = GenerateQuestionFromCVCommandHandler(
-        cv_content=data.cv_content
-    )
-    try:
-        question_generated = await handler.execute(command)
-    except Exception as e:
-        return HTTPException(status_code=400, detail=str(e))
-    return GenerateQuestionsFromCVDto(question=question_generated)
+# @app.post("/gemma/generate-question-from-cv", response_model=GenerateQuestionsFromCVDto)
+# async def gemma_generate_questions_from_cv(data: GenerateQuestionsFromCVSchema):
+#     assert gemma_question_generator is not None
+#     handler = GenerateQuestionFromCVHandler(gemma_question_generator)
+#     command = GenerateQuestionFromCVCommandHandler(
+#         cv_content=data.cv_content
+#     )
+#     try:
+#         question_generated = await handler.execute(command)
+#     except Exception as e:
+#         return HTTPException(status_code=400, detail=str(e))
+#     return GenerateQuestionsFromCVDto(question=question_generated)
